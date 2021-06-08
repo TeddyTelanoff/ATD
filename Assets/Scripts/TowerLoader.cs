bd4b163e0859185a06938a4373ca1a7ddc96e120
Treidex <treidexsemextetry@gmail.com>
2021-06-07 19:38:39 -0700

    WADADAHOOO!

--- Assets/Scripts/TowerLoader.cs
+++ Assets/Scripts/TowerLoader.cs
@@ -1,38 +1,28 @@
 ﻿using System.IO;
 using UnityEngine;
 using Newtonsoft.Json;
-using UnityEngine.SceneManagement;
 
 public partial class TowerManager
 {
 	public void LoadAll()
 	{
-		var towerStats = Directory.GetDirectories(towerStatsLoc);
+		var towerStats = Directory.GetDirectories($"{towerStatsLoc}/Towers");
 		loadedStats = new TowerData[towerStats.Length];
 		for (int i = 0; i < towerStats.Length; i++)
 		{
+			towerStats[i] = towerStats[i].Substring(towerStatsLoc.Length + 1);
+			print(towerStats[i]);
 			loadedStats[i] = Load(towerStats[i]);
 			loadedStats[i].file = towerStats[i];
 		}
 	}
 
-	public Sprite LoadSprite(string path)
-	{
-		if (string.IsNullOrEmpty(path))
-			return null;
-		if (!File.Exists(path))
-			return null;
-
-		byte[] bytes = File.ReadAllBytes(path);
-		Texture2D texture = new Texture2D(1, 1);
-		texture.LoadImage(bytes);
-		Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-		return sprite;
-	}
+	public Sprite LoadSprite(string path) =>
+		Resources.Load<Sprite>(path);
 
 	public TowerData Load(string path)
 	{
-		var data = JsonConvert.DeserializeObject<TowerData>(File.ReadAllText($"{path}/Tower.json"));
+		var data = JsonConvert.DeserializeObject<TowerData>(Resources.Load<TextAsset>($"{path}/Tower").text);
 		LoadTower(path, ref data);
 
 		return data;
@@ -40,33 +30,19 @@ public partial class TowerManager
 
 	public void LoadTower(string path, ref TowerData data)
 	{
-		bool found = false;
-		var scene = SceneManager.GetSceneByPath(path);
-		// TODO: Make it not supa slow
-		foreach (var obj in scene.GetRootGameObjects())
-			if (obj.name == "Model" &&
-				(found = true)) // Neat Trick, huh?
-				{
-					data.mesh = obj;
-					print(obj.scene);
-				}
+		data.mesh = Resources.Load<GameObject>($"{path}/Model");
+		Debug.Log(data.mesh);
 
-		if (!found)
-		{
-			data.mesh = Resources.Load<GameObject>($"{path}/Model");
-			Debug.Log(data.mesh);
-		}
-
-		data._icon = LoadSprite("{path}/Icon.png");
+		data._icon = LoadSprite("{path}/Icon");
 		if (data.path1 != null)
 			for (int i = 0; i < data.path1.Length; i++)
-				data.path1[i].sprite = LoadSprite($"{path}/1-{i + 1}.png");
+				data.path1[i].sprite = LoadSprite($"{path}/1-{i + 1}");
 		if (data.path2 != null)
 			for (int i = 0; i < data.path2.Length; i++)
-				data.path2[i].sprite = LoadSprite($"{path}/2-{i + 1}.png");
+				data.path2[i].sprite = LoadSprite($"{path}/2-{i + 1}");
 		if (data.path3 != null)
 			for (int i = 0; i < data.path3.Length; i++)
-				data.path3[i].sprite = LoadSprite($"{path}/3-{i + 1}.png");
+				data.path3[i].sprite = LoadSprite($"{path}/3-{i + 1}");
 
 		var tmp = new Upgrade[6];
 		if (data.path1 != null)
